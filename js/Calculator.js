cf938606a59dd8c70cf8a45af28936cccb03318b
[cristian] <cristian.c.lopez.m@gmail.com>
2023-11-09 22:01:04 -0500

    fixed the problem with the screen and make simple operations, unless take care about the less operations

--- js/Calculator.js
+++ js/Calculator.js
@@ -6,7 +6,7 @@ let cantidadUsuarios = d.getElementById("usuarios");
 
 let pantalla = d.getElementById("pantalla");
 
-// Numeros 
+// Numeros
 let uno = d.getElementById("uno");
 let dos = d.getElementById("dos");
 let tres = d.getElementById("tres");
@@ -20,28 +20,28 @@ let cero = d.getElementById("cero");
 
 let numbers = [cero, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve];
 
-
-
 // Variables  locales
+let numero1 = null;
+let numero2 = null;
 let contador = 0;
 let contenido = "";
-
+let operacion = ""; // operacion para manipular en la funcion del igual por medio de un switch case.
 
 numbers.forEach((el) => {
-    el.dataset.valor = contador;
-    contador++;
-    console.log(el.dataset.valor)
-})
+  el.dataset.valor = contador;
+  contador++;
+  // console.log(el.dataset.valor)
+});
 
 contador = 0; // reiniciamos el valor del contador
 
 const nuevoUsuario = () => {
-    contador++;
-    if (contador <= 4) cantidadUsuarios.textContent = `${contador}`;
-}
+  contador++;
+  if (contador <= 4) cantidadUsuarios.textContent = `${contador}`;
+};
 
-
-// Operaciones 
+// Operaciones
+// Asignar dataset a las operaciones
 let sumar = d.getElementById("sumar");
 sumar.dataset.operacion = "+";
 let multiplicar = d.getElementById("multiplicar");
@@ -54,47 +54,102 @@ let modulo = d.getElementById("modulo");
 modulo.dataset.operacion = "%";
 let operaciones = [sumar, multiplicar, dividir, restar, modulo];
 
-// Asignar dataset a las operaciones
+const limpiarPantalla = () => (pantalla.textContent = "");
 
-
-
-
-
+let valor = ""; // valor asignado y manipulable.
+const clickNumber = () => {
+  // Verificar si la pantalla está vacía
+  if (pantalla.textContent === "") {
+    console.log(pantalla.textContent);
+    // Iterar a través de los botones de números
+    numbers.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        // Obtener el valor del botón
+        limpiarPantalla();
+        console.log(`pantalla: ${pantalla.textContent}`);
+        valor =
+          valor === ""
+            ? (valor += el.dataset.valor)
+            : (valor += el.dataset.valor);
+        // Mostrar el valor en la pantalla
+        pantalla.textContent += valor;
+        console.log("valor: " + valor);
+      });
+    });
+  } else {
+    // Si la pantalla no está vacía, borrar el contenido
+    limpiarPantalla();
+  }
+};
 
 // Funcion para mostrar los numeros presionados en pantalla
-
 const clickOperation = () => {
-    operaciones.forEach((el) => el.addEventListener("click", (e) => {
-        pantalla.textContent = "";
-        contenido = el.dataset.operacion;
-        if (pantalla.textContent === "") pantalla.textContent = contenido;
-        console.log(pantalla.textContent);
-    }))
-}
-
-
-const clickNumber = () => {
-
-    contenido = "";
-    
-    numbers.forEach((el) => {
-        pantalla.textContent = "";
-        el.addEventListener("click", (e) => {
-            pantalla.textContent = "";
-            if (pantalla.textContent === "") {
-                contenido += `${el.dataset.valor}`;
-                pantalla.textContent = contenido;
-                console.log(pantalla.textContent);
-            }
-
-        })
-    })
-}
+  if (pantalla.textContent !== null || pantalla.textContent !== "") {
+    operaciones.forEach((el) =>
+      el.addEventListener("click", (e) => {
+        operacion = el.dataset.operacion;
+        numero1 = parseInt(pantalla.textContent); // asignamos el valor del  primer numero
+        valor = ""; // devolvemos el valor a simples comillas.
+        // console.log(pantalla.textContent);
+        // console.log("numero 1 "+numero1);
+        limpiarPantalla();
+        contenido = el.dataset.operacion; // asignamos el valor del operador al contenido
+        if (pantalla.textContent === "") pantalla.textContent = contenido; // mostramos el operador en pantalla.
+      })
+    );
+  }
+};
 
 clickNumber();
 clickOperation();
 
+addUsuario.addEventListener("click", (e) => nuevoUsuario());
+
+// Logica para el boton de igual
+const igual = d.getElementById("igual");
+igual.dataset.operacion = "=";
+
+igual.addEventListener("click", (e) => {
+  if (!(pantalla.textContent !== null) || !(pantalla.textContent !== ""))
+    return;
+
+  numero2 = parseInt(pantalla.textContent);
+  let resultado = 0;
+
+  switch (operacion) {
+    case "+":
+      resultado = numero1 + numero2;
+      break;
+    case "-":
+      resultado = numero1 - numero2;
+      break;
+    case "*":
+      resultado = numero1 * numero2;
+      break;
+    case "/":
+      if (numero2 !== 0) {
+        resultado = numero1 / numero2;
+      } else {
+        resultado = "División por cero";
+      }
+      break;
+    default:
+      resultado = "Operador no válido";
+  }
+
+  pantalla.textContent = resultado;
+  // Reiniciamos los valores
+  numero1 = resultado;
+  numero2 = 0;
+
+});
 
 
-
-addUsuario.addEventListener("click", e => nuevoUsuario())
+// limpiar pantalla
+const limpiar = d.getElementById('limpiar');
+limpiar.addEventListener('click', e => {
+    numero1 = 0;
+    numero2 = 0;
+    valor = 0;
+    limpiarPantalla();
+})
